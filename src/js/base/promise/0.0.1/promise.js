0eac031c5937a51fa8f06a894a2fa59a6143357c
陈海云 <yunfour@qq.com>
2014-12-18 14:47:35 +0800

    优化了部分代码

---
+++ src/js/base/promise/0.0.1/promise.js
@@ -0,0 +1,125 @@
+/**
+ * @Author      : 陈海云
+ * @Date        : 2014-12-01
+ * @Memo        : 提供一个实现Promise/Deffer模式的构造函数
+ * 
+ * @param       : 无
+ * 
+ * @methods:
+ *      bind:
+ *          描述: 绑定（订阅）事件；
+ *          参数: 
+ *              eventsName——事件名称（必填），
+ *              callback——事件回调函数（选填，未设置该参数或者该参数为非function类型时则绑定无效）；
+ *          返回值: 当前对象
+ */
+define(function(require) {
+    
+    var createClass = require('base/createClass/1.0.2/createClass'),
+        PubSub      = require('base/pubSub/1.0.3/pubSub');
+    
+    var UNFULFILLED = 0,    // 未完成
+        RESOLVED    = 1,    // 已完成
+        REJECTED    = 2;    // 拒绝
+    
+    var Promise = createClass({
+        superClass: PubSub,
+        init: function() {
+            
+        },
+        methods: {
+            then: function(onResolved, onRejected) {
+                
+                if(typeof onResolved === 'function') {
+                    this.once('resolve', onResolved);
+                }
+                
+                if(typeof onRejected === 'function') {
+                    this.once('reject', onRejected);
+                }
+                
+                return this;
+            }
+        }
+    });
+    
+    var Deffered = createClass({
+        init: function() {
+            var promise = new Promise();
+            
+            this.setAttr('promise', promise);
+            this.setAttr('state', UNFULFILLED);
+        },
+        methods: {
+            resolve: function(data) {
+                
+                var promise = this.getAttr('promise');
+                
+                promise.trigger('resolve', data);
+                
+                this.setAttr('state', RESOLVED);
+                
+                return this;
+            },
+            reject: function(data) {
+                
+                var promise = this.getAttr('promise');
+                
+                promise.trigger('reject', data);
+                
+                this.setAttr('state', REJECTED);
+                
+                return this;
+            }
+        }
+    });
+    
+    
+    
+    
+    var getData = function(callback) {
+        
+        setTimeout(function() {
+            
+            var data = {
+                date: new Date,
+                r: parseInt(Math.random() * 100000)
+            };
+            
+            if(typeof callback === 'function') {
+                callback(data);
+            }
+        }, 5000);
+    };
+    
+    function promisy() {
+        
+        var deffered = new Deffered();
+        
+        getData(function(data) {
+            
+            if(data.r > 10000) {
+                deffered.resolve(data);
+            } else {
+                deffered.reject(data);
+            }
+        });
+        
+        return deffered.getAttr('promise');
+    }
+    
+    promisy().then(
+        function(data) {
+            
+            console.log("Resolve");
+            console.log(data);
+        },
+        function(data) {
+            
+            console.log("Reject");
+            console.log(data);
+        }
+    );
+    
+    return Deffered;
+});
